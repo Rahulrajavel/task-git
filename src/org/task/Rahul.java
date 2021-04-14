21218623fd0b2bb5655860d685373f432da76b20
sreni3 <srenivasen1419@gmail.com>
2021-04-14 22:10:20 +0530

    srenimodified

--- src/org/task/Rahul.java
+++ src/org/task/Rahul.java
@@ -5,10 +5,21 @@ public class Rahul {
 		System.out.println("1");
 	}
 	public void method2() {
-System.out.println("2");
+        System.out.println("2");
 	}
 	public void method3() {
 		System.out.println("3");
+
+	}
+
+	public void method8() {
+		System.out.println("8");
+	}
+	public void method9() {
+        System.out.println("9");
+	}
+	public void method10() {
+		System.out.println("10");
 
 	}
 }
