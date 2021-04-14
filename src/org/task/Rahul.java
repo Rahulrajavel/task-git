052536f04e188edcabe6d9dbf358e965dde8d41d
Rahulrajavel <rrahul0998@gmail.com>
2021-04-14 21:47:03 +0530

    task updated

---
+++ src/org/task/Rahul.java
@@ -0,0 +1,14 @@
+package org.task;
+
+public class Rahul {
+	public void method1() {
+		System.out.println("1");
+	}
+	public void method2() {
+System.out.println("2");
+	}
+	public void method3() {
+		System.out.println("3");
+
+	}
+}
